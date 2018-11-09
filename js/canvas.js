8d21b9992e117e87fa05b71975ba51ac5344445d
Zihan Ye <zaeye1028@gmail.com>
2018-11-08 21:14:10 -0800

    class js added; code readbility improved

---
+++ js/canvas.js
@@ -0,0 +1,170 @@
+/* canvas.js */
+/* draw lines, circles, and poligons on canvas element */
+
+var screenWidth = $(window).width();
+var screenHeight = $(window).height();
+var strokeWidth = 5;
+var svg1 = document.getElementById("svg1"); // get svg1 element
+
+var draw2Vertices = function (svg, points, dash, moveCircleDirection) {
+    var lastIndex = points.length - 1;
+    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'polyline'); //Create a path in SVG's namespace
+    newElement.setAttribute("class", "draw");
+    newElement.setAttribute("points", points);
+    newElement.style.strokeDasharray = dash;
+    newElement.style.strokeDashoffset = dash;
+    svg.appendChild(newElement);
+
+    newElement = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    newElement.setAttribute("class", "socket");
+    newElement.setAttribute("cx", points[lastIndex - 1]);
+    newElement.setAttribute("cy", points[lastIndex] - moveCircleDirection * strokeWidth);
+    newElement.setAttribute("r", strokeWidth);
+    newElement.setAttribute("stroke-width", strokeWidth / 2);
+    svg.appendChild(newElement);
+}
+
+var draw3Vertices = function (svg, points, dash, moveCircleDirection) {
+    var lastIndex = points.length - 1;
+
+    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'polyline'); //Create a path in SVG's namespace
+    newElement.setAttribute("class", "draw");
+    newElement.setAttribute("points", points);
+    newElement.style.strokeDasharray = dash;
+    newElement.style.strokeDashoffset = dash;
+    svg.appendChild(newElement);
+
+    newElement = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    newElement.setAttribute("class", "socket");
+    newElement.setAttribute("cx", points[lastIndex - 1] + 0.5 * Math.sqrt(2) * moveCircleDirection * strokeWidth);
+    newElement.setAttribute("cy", points[lastIndex] + 0.5 * Math.sqrt(2) * strokeWidth);
+    newElement.setAttribute("r", strokeWidth);
+    newElement.setAttribute("stroke-width", strokeWidth / 2);
+    svg.appendChild(newElement);
+}
+
+var draw4Vertices = function (svg, points, dash, moveCircleDirection) {
+    var lastIndex = points.length - 1;
+
+    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'polyline'); //Create a path in SVG's namespace
+    newElement.setAttribute("class", "draw");
+    newElement.setAttribute("points", points);
+    newElement.style.strokeDasharray = dash;
+    newElement.style.strokeDashoffset = dash;
+    svg.appendChild(newElement);
+
+    newElement = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    newElement.setAttribute("class", "socket");
+    newElement.setAttribute("cx", points[lastIndex - 1] + 0.5 * Math.sqrt(2) * moveCircleDirection * strokeWidth);
+    newElement.setAttribute("cy", points[lastIndex] + 0.5 * Math.sqrt(2) * strokeWidth);
+    newElement.setAttribute("r", strokeWidth);
+    newElement.setAttribute("stroke-width", strokeWidth / 2);
+    svg.appendChild(newElement);
+}
+
+var shiftPointsH = function (points, length) {
+    for (var i = 0; i < points.length; i++) {
+        if (i % 2 == 0) {
+            points[i] += length;
+        }
+    }
+    return points;
+}
+
+var symmetryH = function (points) {
+    for (var i = 0; i < points.length; i++) {
+        if (i % 2 == 0) {
+            points[i] = screenWidth - points[i];
+        }
+    }
+    return points;
+}
+
+var reverse = function (points) {
+    var reversePoints = [];
+    for (var i = 0; i < points.length - 1; i += 2) {
+        var x = points[i];
+        var y = points[i + 1];
+        reversePoints.unshift(y);
+        reversePoints.unshift(x);
+    }
+    return reversePoints;
+}
+
+var chipMain = new Chip('chipMain');
+// alert(chipMain.topLeft);
+var division = 9;
+var deltaChipMainX = chipMain.width / division;
+
+/*Bottom Center Circuit*/
+var drawBottomSide ={
+    draw:function(){
+        var dash = screenHeight - chipMain.bottom-chipMain.border;
+        var points = [screenWidth/2 - deltaChipMainX, screenHeight,
+            screenWidth/2 - deltaChipMainX, chipMain.bottom+chipMain.border];
+        for (var count = 0; count < 3; count++) {
+            draw2Vertices(svg1, points, dash, 1);
+            points = shiftPointsH(points, deltaChipMainX);
+        }
+    }
+}
+
+drawBottomSide.draw();
+
+var drawLeftSide={
+    draw:function(){
+        var x1 = chipMain.left-chipMain.border;
+        var y1 = chipMain.top;
+        var x2 = x1-deltaChipMainX;
+        var y2 = y1- deltaChipMainX;
+        var x3 = x2;
+        var y3 = 0;
+
+        // alert(screenHeight);
+        var points = [x3, y3,
+            x2, y2,
+            x1, y1]
+
+        dash = y2 + Math.sqrt(2) * deltaChipMainX;
+        draw3Vertices(svg1, points, dash, 1);
+    },
+    draw2:function(){
+        var x1 = chipMain.left-chipMain.border;
+        var y1 = chipMain.top+deltaChipMainX;
+        var x2 = x1 - 2*deltaChipMainX;
+        var y2 = y1 - 2*deltaChipMainX;
+        var x3 = x2;
+        var y3 = y2 - 2 * deltaChipMainX;
+        var x4 = x3 - 2 * deltaChipMainX;
+        var y4 = 0;
+
+        var points = [x4, y4,
+            x3, y3,
+            x2, y2,
+            x1, y1];
+
+        dash = 2 * Math.sqrt(2) * 2 * deltaChipMainX + y2-y3;
+        draw4Vertices(svg1, points, dash, 1);
+    },
+    draw3:function(){
+        var x1 = chipMain.left-chipMain.border;
+        var y1 = chipMain.top+2*deltaChipMainX;
+        var x2 = x1 - 3*deltaChipMainX;
+        var y2 = y1 - 3*deltaChipMainX;
+        var x3 = x2;
+        var y3 = y2 - 2 * deltaChipMainX;
+        var x4 = x3 - 2 * deltaChipMainX;
+        var y4 = 0;
+
+        var points = [x4, y4,
+            x3, y3,
+            x2, y2,
+            x1, y1];
+
+        dash = (2+3) * Math.sqrt(2) * deltaChipMainX + y2-y3;
+        draw4Vertices(svg1, points, dash, 1);
+    }
+}
+drawLeftSide.draw();
+drawLeftSide.draw2();
+drawLeftSide.draw3();
---
+++ js/class.js
@@ -0,0 +1,42 @@
+class Chip {
+    constructor(id) {
+        this.id = id;
+        this.getOffset();
+    }
+
+    getOffset(id = this.id) {
+        this.border = 25;
+        this.offset = $('#' + id).offset();
+        this.width = $('#' + id).width();
+        this.height = $('#' + id).height();
+        this.getOffsetDetail();
+    }
+
+    getOffsetDetail(offset = this.offset, border = this.border) {
+        this.left = offset.left;
+        // this.left = offset.left - border;
+        this.right = screenWidth - this.left;
+        this.top = offset.top;
+        // this.top = offset.top - border;
+        this.bottom = screenHeight - this.top;
+        this.getCorner();
+    }
+
+    getCorner() {
+        this.topLeft = new Point(this.left, this.top);
+        this.topRight = new Point(this.right, this.top);
+        this.botLeft = new Point(this.left, this.bottom);
+        this.botRight = new Point(this.right, this.bottom);
+    }
+}
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    toString() {
+        return this.x + " " + this.y;
+    }
+}
