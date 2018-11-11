86a0f13edf8a2c9d2f2751c96cc680f7815f4108
Zihan Ye <zaeye1028@gmail.com>
2018-11-11 11:07:18 -0800

    tranform cannonball, explosion, and particles from function to class

--- js/canvas.js
+++ js/canvas.js
@@ -30,6 +30,8 @@ const fps = 60;
 const lambda = second * fps;
 const period = 20;
 const gravity = 0.5;
+const bounceTimes = 2;
+const explosionParts = 2;
 
 /* method for drawing polygons on bottom side */
 var drawBottomSide = {
@@ -217,163 +219,11 @@ canvas.height = screenHeight;
 var cannonballs = [];
 var explosions = [];
 
-function Particle(x, y, dx, dy, radius, color) {
-  this.x = x;
-  this.y = y;
-  this.dx = dx;
-  this.dy = -dy;
-  this.radius = 5;
-  this.color = color;
-  this.timeToLive = 3;
-  this.mass = 0.2;
-  
-
-  this.update = function () {
-    if (this.y + this.radius + this.dy > canvas.height) {
-      this.dy = -(this.dy)/2;
-      this.timeToLive--;
-    }
-
-    if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius + this.dx < 0) {
-      this.dx = -this.dx;
-    }
-    this.dy += gravity * this.mass;
-    this.x += this.dx;
-    this.y += this.dy;
-    this.draw();
-  };
-
-  this.draw = function () {
-    c.save();
-    c.beginPath();
-    c.arc(this.x, this.y, 2, 0, Math.PI * 2, false);
-    c.shadowColor = this.color;
-    c.shadowBlur = 10;
-    c.shadowOffsetX = 0;
-    c.shadowOffsetY = 0;
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
-    c.restore();
-  };
-}
-
-function Explosion(cannonball) {
-  this.particles = [];
-  this.rings = [];
-  this.source = cannonball;
-
-  this.init = function () {
-    for (var i = 0; i < 3; i++) {
-
-      var dx = (Math.random() * 6) - 3;
-      var dy = (Math.random() * 6) - 3;
-
-      // var hue = (255 / 5) * i;
-      // var color = "hsl(" + hue + ", 100%, 50%)";
-      // var randomColorIndex = Math.floor(Math.random() * this.source.particleColors.length);
-      // var randomParticleColor = this.source.particleColors[randomColorIndex];
-      var randomParticleColor = "white";
-      this.particles.push(new Particle(this.source.x, this.source.y, dx, dy, 1, randomParticleColor));
-    }
-
-    // Create ring once explosion is instantiated
-    // this.rings.push(new Ring(this.source, "blue"));
-  };
-
-  this.init();
-
-  this.update = function () {
-    for (var i = 0; i < this.particles.length; i++) {
-      this.particles[i].update();
-
-      // Remove particles from scene one time to live is up
-      if (this.particles[i].timeToLive < 0) {
-        this.particles.splice(i, 1);
-      }
-    }
-
-    // Render rings
-    for (var j = 0; j < this.rings.length; j++) {
-      this.rings[j].update();
-
-      // Remove rings from scene one time to live is up
-      if (this.rings[j].timeToLive < 0) {
-        this.rings.splice(i, 1);
-      }
-    }
-  };
-}
-
-function Cannonball(x, y, radius, color, points, dash) {
-  this.x = x;
-  this.y = y;
-  this.radius = radius;
-  this.color = color;
-  this.points = points;
-  this.dash = dash;
-  this.destroy = false;
-  
-
-  this.init = function () {
-    // Initialize the cannonballs start coordinates (from muzzle of cannon)
-    this.currIndex = 0;
-    this.lastIndex = points.length - 1;
-    this.x = points[0].x;
-    this.y = points[0].y;
-    this.d = distance(points[this.currIndex + 1], points[this.currIndex]);
-    this.dx = (points[this.currIndex + 1].x - points[this.currIndex].x) * (this.dash/this.d) / lambda;
-    this.dy = (points[this.currIndex + 1].y - points[this.currIndex].y) * (this.dash/this.d)/ lambda;
-  };
-
-  this.update = function () {
-    if(this.currIndex<this.lastIndex){
-      if (Math.abs(this.x - points[this.currIndex+1].x > 1 ||
-        Math.abs(this.y - points[this.currIndex+1].y) > 1)){
-          //nothing
-        this.d = distance(points[this.currIndex + 1], points[this.currIndex]);
-        this.dx = (points[this.currIndex + 1].x - points[this.currIndex].x) *(this.dash/this.d) / lambda;
-        this.dy = (points[this.currIndex + 1].y - points[this.currIndex].y) *(this.dash/this.d)/ lambda;
-      } else{
-        this.currIndex++;
-      }
-    }else{
-      this.destroy = true;
-    }
-   
-    this.x += this.dx;
-    this.y += this.dy;
-
-    this.draw();
-  };
-
-  this.draw = function () {
-    c.save();
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.shadowColor = this.color;
-    c.shadowBlur = 5;
-    c.shadowOffsetX = 0;
-    c.shadowOffsetY = 0;
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
-    c.restore();
-  };
-
-  this.init();
-}
-
-// alert(pointsArray[0] +" "+ pointsArray[1]);
 var end = false;
 var count = 0;
 function animate() {
   window.requestAnimationFrame(animate);
 
-  // alert(svg1.getAttribute('stroke-dashoffset') );
-
-  //   c.fillStyle = "rgba(18, 18, 18, 0.2)";
-  //   c.fillRect(0, 0, canvas.width, canvas.height);		
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   if (!end) {
--- js/class.js
+++ js/class.js
@@ -55,3 +55,156 @@ class Point {
         return this.x + "," + this.y;
     }
 }
+
+class Particle{
+    constructor(x, y, dx, dy, radius, color){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = -dy;
+        this.radius = 5;
+        this.color = color;
+        this.timeToLive = bounceTimes;
+        this.mass = 0.2;
+    }
+  
+    update(){
+      if (this.y + this.radius + this.dy > canvas.height) {
+        this.dy = -(this.dy)/2;
+        this.timeToLive--;
+      }
+  
+      if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius + this.dx < 0) {
+        this.dx = -this.dx;
+      }
+      this.dy += gravity * this.mass;
+      this.x += this.dx;
+      this.y += this.dy;
+      this.draw();
+    };
+  
+    draw() {
+      c.save();
+      c.beginPath();
+      c.arc(this.x, this.y, 2, 0, Math.PI * 2, false);
+      c.shadowColor = this.color;
+      c.shadowBlur = 10;
+      c.shadowOffsetX = 0;
+      c.shadowOffsetY = 0;
+      c.fillStyle = this.color;
+      c.fill();
+      c.closePath();
+      c.restore();
+    };
+  }
+
+//   function Explosion(cannonball) {
+class Explosion {
+    constructor(cannonball){
+        this.particles = [];
+        this.rings = [];
+        this.source = cannonball;
+        this.init();
+    }
+    init() {
+      for (var i = 0; i < explosionParts; i++) {
+        var dx = (Math.random() * 6) - 3;
+        var dy = (Math.random() * 6) - 3;
+  
+        // var hue = (255 / 5) * i;
+        // var color = "hsl(" + hue + ", 100%, 50%)";
+        // var randomColorIndex = Math.floor(Math.random() * this.source.particleColors.length);
+        // var randomParticleColor = this.source.particleColors[randomColorIndex];
+        var randomParticleColor = "aqua";
+        this.particles.push(new Particle(this.source.x, this.source.y, dx, dy, 1, randomParticleColor));
+      }
+  
+      // Create ring once explosion is instantiated
+      // this.rings.push(new Ring(this.source, "blue"));
+    };
+  
+  
+    update() {
+      for (var i = 0; i < this.particles.length; i++) {
+        this.particles[i].update();
+  
+        // Remove particles from scene one time to live is up
+        if (this.particles[i].timeToLive < 0) {
+          this.particles.splice(i, 1);
+        }
+      }
+  
+      // Render rings
+      for (var j = 0; j < this.rings.length; j++) {
+        this.rings[j].update();
+  
+        // Remove rings from scene one time to live is up
+        if (this.rings[j].timeToLive < 0) {
+          this.rings.splice(i, 1);
+        }
+      }
+    };
+  }
+  
+  class Cannonball{
+  
+    constructor (x, y, radius, color, points, dash) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.points = points;
+    this.dash = dash;
+    this.destroy = false;
+    this.init();
+
+    }
+    
+  
+    init () {
+      // Initialize the cannonballs start coordinates (from muzzle of cannon)
+      this.currIndex = 0;
+      this.lastIndex = this.points.length - 1;
+      this.x = this.points[0].x;
+      this.y = this.points[0].y;
+      this.d = distance(this.points[this.currIndex + 1], this.points[this.currIndex]);
+      this.dx = (this.points[this.currIndex + 1].x - this.points[this.currIndex].x) * (this.dash/this.d) / lambda;
+      this.dy = (this.points[this.currIndex + 1].y - this.points[this.currIndex].y) * (this.dash/this.d)/ lambda;
+    };
+  
+    update () {
+      if(this.currIndex<this.lastIndex){
+        if (Math.abs(this.x - this.points[this.currIndex+1].x > 1 ||
+          Math.abs(this.y - this.points[this.currIndex+1].y) > 1)){
+            //nothing
+          this.d = distance(this.points[this.currIndex + 1], this.points[this.currIndex]);
+          this.dx = (this.points[this.currIndex + 1].x - this.points[this.currIndex].x) *(this.dash/this.d) / lambda;
+          this.dy = (this.points[this.currIndex + 1].y - this.points[this.currIndex].y) *(this.dash/this.d)/ lambda;
+        } else{
+          this.currIndex++;
+        }
+      }else{
+        this.destroy = true;
+      }
+     
+      this.x += this.dx;
+      this.y += this.dy;
+  
+      this.draw();
+    };
+  
+    draw() {
+      c.save();
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.shadowColor = this.color;
+      c.shadowBlur = 5;
+      c.shadowOffsetX = 0;
+      c.shadowOffsetY = 0;
+      c.fillStyle = this.color;
+      c.fill();
+      c.closePath();
+      c.restore();
+    };
+  
+  }
